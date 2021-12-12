cd7de21a75dfc60524778677fd90d8f5596fe704
Amir <Olimov.amir@mail.ru>
2021-12-12 14:56:06 +0500

    created const resultSum

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ document.body.innerHTML = '<h1> JavaScript Hi Git </h1>'
 
 const sum = [1, 2, 3, 5];
 
-const resultSum = sum.reduce((previous, current) =>{
-    return  previous + current
-    },0);
+const resultSum = sum.reduce((previous, current) =>previous + current,0);
 console.log(resultSum);
 
 console.log('test')
