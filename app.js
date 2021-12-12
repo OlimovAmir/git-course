dc6eaad04857daeb4f592fca10447fea8fa1b4ec
Amir <Olimov.amir@mail.ru>
2021-12-12 11:27:07 +0500

    created new file

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const sum = [1, 2, 3, 5];
+const sum1 = [1, 2, 3, 5];
 
-const resultSum = sum.reduce((previous, current) =>{
+const resultSum1 = sum1.reduce((previous, current) =>{
     return  previous + current
     },0);
-console.log(resultSum);
+console.log(resultSum1);
