77360b4025a5103524f818073f97d1add4f26048
Amir <Olimov.amir@mail.ru>
2021-12-12 11:25:25 +0500

    add

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 document.body.innerHTML = '<h1> JavaScript Hi Git </h1>'
 
+const sum = [1, 2, 3, 5];
+
+const resultSum = sum.reduce((previous, current) =>{
+    return  previous + current
+    },0);
+console.log(resultSum);
